349d6c4c84d6e6d95f2acea0a7c75cabab74cf3e
maverick-farhan <mdfarhanofficial@gmail.com>
2023-11-15 12:01:42 +0530

    Finally mongo atlas connected

--- DB/configDB.js
+++ DB/configDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const connect =()=>{
-  mongoose.connect('mongodb://127.0.0.1:27017/quiz')
+  mongoose.connect('mongodb+srv://euvDKyllgi7xzTAC:euvDKyllgi7xzTAC@cluster0.tbrppdl.mongodb.net/quiz?retryWrites=true&w=majority')
 .then(()=>{
   console.log("connected")})
 }
